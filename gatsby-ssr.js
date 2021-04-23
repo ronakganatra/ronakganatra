b366c7076970a4d21a3b5f4a38dd81e245e07df1
Ronak Ganatra <20298819+ronakganatra@users.noreply.github.com>
2021-04-23 18:16:57 +0200

    Apply Larisa hack for SEO

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,1 +1,18 @@
 export { wrapPageElement, wrapRootElement } from './gatsby-browser'
+
+// renders meta tags before inline style to ensure correct link unfurling on social media
+export const onPreRenderHTML = ({
+    getHeadComponents,
+    replaceHeadComponents,
+  }) => {
+    const headComponents = getHeadComponents();
+    headComponents.sort((x, y) => {
+      if (x.props && x.props['data-react-helmet']) {
+        return -1;
+      } else if (y.props && y.props['data-react-helmet']) {
+        return 1;
+      }
+      return 0;
+    });
+    replaceHeadComponents(headComponents);
+  };
